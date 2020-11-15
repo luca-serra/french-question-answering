c4726f901a8ee118d515bc1fc815a0bbe7eb006f
luca-serra <luca.serra@student.ecp.fr>
2020-11-15 16:08:23 +0100

    TFIDF with norm imposed

--- src/tfidf.py
+++ src/tfidf.py
@@ -8,32 +8,17 @@ import time
 from sklearn.feature_extraction.text import TfidfVectorizer
 from utils import utils
 import fr_core_news_sm
+import random
 
 nlp = fr_core_news_sm.load()
 
-stop_words = nlp.Defaults.stop_words
+# stop_words = nlp.Defaults.stop_words
 
 with open('../datasets/data/contexts.json', encoding="utf8") as json_file:
     contexts = json.load(json_file)
 
 with open('../datasets/data/questions.json', encoding="utf8") as json_file:
     questions = json.load(json_file)
-
-# for question in questions['data']:
-start_time = time.time()
-
-question = questions['data'][0]['question']
-context_id = questions['data'][0]['context_id']
-
-# tokenizer = TfidfVectorizer(lowercase=True).build_tokenizer()
-tokenizer = utils.spacy_tokenizer
-vocabulary = tokenizer(question)
-vectorizer = TfidfVectorizer(vocabulary=vocabulary, stop_words=stop_words)
-# print([tuple['context'] for tuple in contexts['data'][:5]])
-x = vectorizer.fit_transform([question] + [tuple['context'] for tuple in contexts['data'][:]])
-x = x.toarray()
-# features = np.zeros((3, 2))
-# print(features)
 
 
 def build_features(row):
@@ -46,20 +31,76 @@ def build_features(row):
     return [np.mean(row), c / row.shape[0]]
 
 
-features = []
-for row_idx in range(x.shape[0]):
-    features.append(build_features(x[row_idx, :]))
+context_ids = [tuple['context_id'] for tuple in contexts['data']]
 
-features = np.array(features)
+n = 100
+c = 0
+c_2 = 0
+c_top_avg = 0
+c_top_ratio = 0
+inter = 0
+random.Random(1).shuffle(questions['data'])
+for q in questions['data'][:n]:
+    pred_true = False
 
-print(vectorizer.get_feature_names())
-# print(x)
-print('idf')
-print(features)
-print(np.argmax(features[1:, 0]))
-print(np.argmax(features[1:, 1]))
-print(vocabulary)
-print(f'{time.time() - start_time:.2f} sec')
+    start_time = time.time()
 
+    question = q['question']
+    context_id = q['context_id']
+    corpus = [question] + [tuple['context'] for tuple in contexts['data'][:]]
+    # tokenizer = TfidfVectorizer(lowercase=True).build_tokenizer()
+    # tokenizer = utils.spacy_tokenizer
+    tokenizer = utils.custom_tokenizer
+    vocabulary = tokenizer(question, remove_duplicate=True)
+    vectorizer = TfidfVectorizer(vocabulary=vocabulary, tokenizer=tokenizer, sublinear_tf=True)
+    # print([tuple['context'] for tuple in contexts['data'][:5]])
+    x = vectorizer.fit_transform(corpus)
+    x = x.toarray()
+    # features = np.zeros((3, 2))
+    # print(features)
+
+    features = []
+    for row_idx in range(x.shape[0]):
+        features.append(build_features(x[row_idx, :]))
+
+    features = np.array(features)
+    print(question)
+    print(vocabulary)
+    # print(features[:5, :])
+    indices_avg = np.argpartition(features[1:, 0], -5)[-5:]
+    indices_ratio = np.argpartition(features[1:, 1], -5)[-5:]
+    predicted_contexts_avg = [contexts['data'][idx]['context_id'] for idx in indices_avg]
+    predicted_contexts_ratio = [contexts['data'][idx]['context_id'] for idx in indices_ratio]
+    predicted_context = contexts['data'][np.argmax(features[1:, 0])]['context_id']
+    predicted_context_2 = contexts['data'][np.argmax(features[1:, 1])]['context_id']
+    # print(predicted_context)
+    # print(predicted_context_2)
+    # print(predicted_contexts_avg)
+    # print(predicted_contexts_ratio)
+    # print(context_id)
+    if context_id == predicted_context:
+        c += 1
+        pred_true = True
+    if context_id == predicted_context_2:
+        c_2 += 1
+        pred_true = True
+    if pred_true:
+        inter += 1
+    if context_id in predicted_contexts_avg:
+        print('OK')
+        c_top_avg += 1
+    else:
+        print(predicted_contexts_avg)
+        for idx in indices_avg:
+            print(x[1 + idx, :])
+        print(context_id)
+        print(x[1 + context_ids.index(context_id), :])
+    if context_id in predicted_contexts_ratio:
+        c_top_ratio += 1
+    print(f'{time.time() - start_time:.2f} sec')
+
+print(
+    f'n: {n}, ratio_avg: {c/n}, ratio_custom:{c_2/n}, ratio_inter: {inter/n}, accuracy top5 avg: {c_top_avg/n}, accuracy top5 ratio: {c_top_ratio/n}'
+)
 
 # ['test de mon algorithme', 'ceci est un test à tester', "Ma phrase de test doit être testée (et non pas tester) comme ceci, j'attends le résultat. Chanteur chanter chant ! Petit test; OK. L'apostrophe"]
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,10 +1,15 @@
 import fr_core_news_sm
 import string
+import re
 
 nlp = fr_core_news_sm.load()
-punctuations = string.punctuation
-stop_words = nlp.Defaults.stop_words
-tokenizer = nlp.Defaults.create_tokenizer(nlp)
+PUNCTUATIONS = string.punctuation.replace("'", '').replace('[', '').replace(']', '')
+PUNCTUATIONS = '[' + ' '.join(PUNCTUATIONS) + ' « »' + ']'
+STOP_WORDS = nlp.Defaults.stop_words
+ADDED_STOP_WORDS = ['t', 'y', "s'", "l'"]
+for word in ADDED_STOP_WORDS:
+    STOP_WORDS.add(word)
+# tokenizer = nlp.Defaults.create_tokenizer(nlp)
 
 
 def spacy_tokenizer(sentence):
@@ -19,26 +24,69 @@ def spacy_tokenizer(sentence):
     tokens = tokenizer(sentence)
     # tokens = [word.lemma_ for word in tokens]
     tokens = [word.lower_ for word in tokens]
-    tokens = [
-        word for word in tokens if not (str(word) in punctuations or str(word) in stop_words)
-    ]
+    # tokens = [
+    #     word for word in tokens if not (str(word) in punctuations or str(word) in STOP_WORDS)
+    # ]
     return tokens
 
 
-def compute_tfidf_matrix(corpus, vocabulary, tokenizer):
-    scores_row = []
+# def custom_tokenizer(sentence, remove_duplicate=False):
+#     tokens = ' '.join(re.sub(PUNCTUATIONS, ' ', sentence.lower()).split()).split(' ')
+#     tokens_clean = []
+#     for token in tokens:
+#         if "'" in token:
+#             split = token.split("'")
+#             if split[0] != "":
+#                 tokens_clean.append(split[0] + "'")
+#             if split[1] != "":
+#                 tokens_clean.append(split[1])
+#         else:
+#             tokens_clean.append(token)
 
-    def computeonrow(document):
-        tokens = tokenizer(document)
-        res = {}
-        for word in vocabulary:
-            res[word] = tokens.count(word)
-        return res
+#     if remove_duplicate:
+#         tokens_clean = list(set(tokens_clean))
 
-    for document in corpus:
-        scores_row.append(computeonrow(document))
+#     tokens_clean = [
+#         word for word in tokens_clean if not (str(word) in STOP_WORDS)
+#     ]
+#     return tokens_clean
 
-    return scores_row
+# def custom_stemmer(token):
+#     endings = ['er', 'e', 'ant', 'ez', 'ent', 'é']
+#     for ending in endings:
+#         if token.endswith(ending):
+#             return token[:-len(ending)]
+#     return token
+
+
+def custom_tokenizer(sentence, remove_duplicate=False):
+    tokens = re.sub(PUNCTUATIONS, ' ', sentence.lower())
+    tokens = tokens.replace('-', ' ').split()
+    tokens_clean = []
+    for token in tokens:
+        # token = custom_stemmer(token)
+        if "'" in token:
+            split = token.split("'")
+            if split[0] != "":
+                token = split[0] + "'"
+                if not (token in STOP_WORDS):
+                    tokens_clean.append(token)
+            if split[1] != "":
+                if not (split[1] in STOP_WORDS):
+                    if split[1][-1] == 's':
+                        token = split[1][:-1]
+                    tokens_clean.append(split[1])
+        else:
+            if not (token in STOP_WORDS):
+                if token[-1] == 's':
+                    token = token[:-1]
+                tokens_clean.append(token)
+    if remove_duplicate:
+        tokens_clean = list(set(tokens_clean))
+    # tokens_clean = [
+    #     word for word in tokens_clean if not (word in STOP_WORDS)
+    # ]
+    return tokens_clean
 
 
 if __name__ == '__main__':
@@ -46,14 +94,18 @@ if __name__ == '__main__':
     #     "Ma phrase de test doit être testée (et non pas tester) comme ceci, j'attends le résultat. Chanteur chanter chant ! Petit test; OK. L'apostrophe"
     # )
     # print(res)
-    context1 = "L'idée selon laquelle une planète inconnue pourrait exister entre les orbites de Mars et Jupiter fut proposée pour la première fois par Johann Elert Bode en 1768. Ses suggestions étaient basées sur la loi de Titius-Bode, une théorie désormais obsolète proposée par Johann Daniel Titius en 1766,. Selon cette loi, le demi-grand axe de cette planète aurait été d'environ 2,8 ua. La découverte d'Uranus par William Herschel en 1781 accrut la confiance dans la loi de Titius-Bode et, en 1800, vingt-quatre astronomes expérimentés combinèrent leurs efforts et entreprirent une recherche méthodique de la planète proposée,. Le groupe était dirigé par Franz Xaver von Zach. Bien qu'ils n'aient pas découvert Cérès, ils trouvèrent néanmoins plusieurs autres astéroïdes."
-    context2 = "Piazzi observa Cérès 24 fois, la dernière fois le 11 février. Le 24 janvier 1801, Piazzi annonça sa découverte par des lettres à plusieurs collègues italiens, parmi lesquels Barnaba Oriani à Milan. Il la décrivit comme une comète, mais remarqua que « puisque son mouvement est lent et uniforme, il m'a semblé à plusieurs reprises qu'il pourrait s'agir de quelque chose de mieux qu'une comète. » En avril, Piazzi envoya ses observations complètes à Oriani, Bode et Lalande à Paris. Elles furent publiées dans l'édition de septembre 1801 du Monatliche Correspondenz."
-    context3 = "Peu après sa découverte, Cérès s'approcha trop près du Soleil et ne put être observée à nouveau ; les autres astronomes ne purent confirmer les observations de Piazzi avant la fin de l'année. Cependant, après une telle durée, il était difficile de prédire la position exacte de Cérès. Afin de retrouver l'astéroïde, Carl Friedrich Gauss développa une méthode de déduction de l'orbite basée sur trois observations. En l'espace de quelques semaines, il prédit celle de Cérès et communiqua ses résultats à Franz Xaver von Zach, éditeur du Monatliche Correspondenz. Le 31 décembre 1801, von Zach et Heinrich Olbers confirmèrent que Cérès avait été retrouvée près de la position prévue, validant ainsi la méthode."
-    question1_1 = (
-        "Quel astronome a émit l'idée en premier d'une planète entre les orbites de Mars et Jupiter ?"
-    )
-    corpus = [question1_1, context1, context2, context3]
-    tokenizer = lambda x: x.split(' ')#spacy_tokenizer
-    vocabulary = spacy_tokenizer(question1_1)
-    print(compute_tfidf_matrix(corpus, vocabulary, tokenizer))
-
+    # context1 = "L'idée selon laquelle une planète inconnue pourrait exister entre les orbites de Mars et Jupiter fut proposée pour la première fois par Johann Elert Bode en 1768. Ses suggestions étaient basées sur la loi de Titius-Bode, une théorie désormais obsolète proposée par Johann Daniel Titius en 1766,. Selon cette loi, le demi-grand axe de cette planète aurait été d'environ 2,8 ua. La découverte d'Uranus par William Herschel en 1781 accrut la confiance dans la loi de Titius-Bode et, en 1800, vingt-quatre astronomes expérimentés combinèrent leurs efforts et entreprirent une recherche méthodique de la planète proposée,. Le groupe était dirigé par Franz Xaver von Zach. Bien qu'ils n'aient pas découvert Cérès, ils trouvèrent néanmoins plusieurs autres astéroïdes."
+    # context2 = "Piazzi observa Cérès 24 fois, la dernière fois le 11 février. Le 24 janvier 1801, Piazzi annonça sa découverte par des lettres à plusieurs collègues italiens, parmi lesquels Barnaba Oriani à Milan. Il la décrivit comme une comète, mais remarqua que « puisque son mouvement est lent et uniforme, il m'a semblé à plusieurs reprises qu'il pourrait s'agir de quelque chose de mieux qu'une comète. » En avril, Piazzi envoya ses observations complètes à Oriani, Bode et Lalande à Paris. Elles furent publiées dans l'édition de septembre 1801 du Monatliche Correspondenz."
+    # context3 = "Peu après sa découverte, Cérès s'approcha trop près du Soleil et ne put être observée à nouveau ; les autres astronomes ne purent confirmer les observations de Piazzi avant la fin de l'année. Cependant, après une telle durée, il était difficile de prédire la position exacte de Cérès. Afin de retrouver l'astéroïde, Carl Friedrich Gauss développa une méthode de déduction de l'orbite basée sur trois observations. En l'espace de quelques semaines, il prédit celle de Cérès et communiqua ses résultats à Franz Xaver von Zach, éditeur du Monatliche Correspondenz. Le 31 décembre 1801, von Zach et Heinrich Olbers confirmèrent que Cérès avait été retrouvée près de la position prévue, validant ainsi la méthode."
+    # question1_1 = "Quel astronome a émit l'idée en premier d'une planète entre les orbites de Mars et Jupiter ?"
+    # corpus = [question1_1, context1, context2, context3]
+    # # print(spacy_tokenizer('y a-t-il'))
+    # # vocabulary = spacy_tokenizer(question1_1)
+    # # print(compute_tfidf_matrix(corpus, vocabulary, tokenizer))
+    # print(corpus)
+    # tokenizer = nlp.Defaults.create_tokenizer(nlp)
+    # tokenized_corpus = []
+    # for doc in tokenizer.pipe(corpus, batch_size=50):
+    #     tokenized_corpus.append([doc])
+    # print(tokenized_corpus)
+    print(PUNCTUATIONS)
