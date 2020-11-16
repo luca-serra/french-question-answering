1a91ea3b8adb4a24deec01674baaa6cbdfe07718
luca-serra <luca.serra@student.ecp.fr>
2020-11-16 10:37:54 +0100

    Cleaned TFIDF results display

--- illuin/models/tfidf.py
+++ illuin/models/tfidf.py
@@ -1,15 +1,12 @@
 """
 Implementing a TFIDF approach to find the best context for a given question
 """
-import json
-import random
 import time
-
+import random as ra
 import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.feature_extraction.text import TfidfVectorizer
-from utils import custom_tokenizer, utils
-from illuin.src.utils import file_creation
+from illuin.utils import custom_tokenizer, utils, question_and_context
 
 
 class TfidfClassifier(BaseEstimator):
@@ -20,84 +17,76 @@ class TfidfClassifier(BaseEstimator):
     def fit(self, X, y=None):
         return self  # for TFIDF model, there is no fit step
 
-    def predict(self, file=True, n=-1, question=None):
+    def predict(self, file=True, n=-1, random=False, question=None, verbose=False):
         if file:
-            questions, contexts = file_creation.build_questions_and_contexts(self.filename)
+            questions, contexts = question_and_context.build_question_and_context(self.filename)
+            if n > 0:
+                questions = questions[:n]
+            if random:
+                ra.shuffle(questions)
         else:
-            _, contexts = file_creation.build_questions_and_contexts(self.filename)
+            _, contexts = question_and_context.build_question_and_context(self.filename)
             questions = [{'question': question, 'question_id': 0, 'context_id': None}]
 
+        context_ids = [tuple['context_id'] for tuple in contexts]
 
-context_ids = [tuple['context_id'] for tuple in contexts['data']]
+        count_avg = 0  # counting the number of correct predictions based on the average of TDFIDF matrix (row)
+        count_ratio = 0  # counting the number of correct predictions based on the ratio of non-zero terms in the row
+        count_top5_avg = 0  # same but with a top5 prediction
+        count_top5_ratio = 0  # same but with a top5 prediction
+        for q in questions:
+            start_time = time.time()
+            question = q['question']
+            context_id = q['context_id']
+            corpus = [question] + [tuple['context'] for tuple in contexts]
+            tokenizer = custom_tokenizer.custom_tokenizer
+            vocabulary = tokenizer(question, remove_duplicate=True)
+            vectorizer = TfidfVectorizer(
+                vocabulary=vocabulary, tokenizer=tokenizer, norm=None, binary=True
+            )
+            tfidf_matrix = vectorizer.fit_transform(corpus)
+            tfidf_matrix = tfidf_matrix.toarray()
 
-n = 100
-c = 0
-c_2 = 0
-c_top_avg = 0
-c_top_ratio = 0
-inter = 0
-random.shuffle(questions['data'])
-for q in questions['data'][:n]:
-    pred_true = False
+            features = utils.build_features(tfidf_matrix)
+            if verbose:
+                print(f'Question: {question}')
+                print(f'Vocabulary: {vocabulary}')
+            indices_top5_avg = np.argpartition(features[1:, 0], -5)[-5:]
+            indices_top5_ratio = np.argpartition(features[1:, 1], -5)[-5:]
+            predicted_contexts_top5_avg = [contexts[idx]['context_id'] for idx in indices_top5_avg]
+            predicted_contexts_top5_ratio = [
+                contexts[idx]['context_id'] for idx in indices_top5_ratio
+            ]
+            predicted_context_avg = contexts[np.argmax(features[1:, 0])]['context_id']
+            predicted_context_ratio = contexts[np.argmax(features[1:, 1])]['context_id']
+            if context_id == predicted_context_avg:
+                count_avg += 1
+            if context_id == predicted_context_ratio:
+                count_ratio += 1
+            if context_id in predicted_contexts_top5_avg:
+                if verbose:
+                    print('✓')
+                count_top5_avg += 1
+            else:
+                if verbose:
+                    print(predicted_contexts_top5_avg)
+                    if not (context_id is None):
+                        print('✘')
+                        print(context_id)
+                        print(tfidf_matrix[1 + context_ids.index(context_id), :])
+                    else:
+                        print('↑ top 5 suggested contexts for the question')
+            if context_id in predicted_contexts_top5_ratio:
+                count_top5_ratio += 1
+            if verbose:
+                print(f'Answered in {time.time() - start_time:.2f} sec')
 
-    start_time = time.time()
+        if file:
+            print(
+                '----------------------------------------------------------------\n',
+                f'Number of questions: {n}, accuracy_avg: {count_avg/n}, accuracy_ratio: {count_ratio/n},\
+            accuracy top5 avg: {count_top5_avg/n}, accuracy top5 ratio: {count_top5_ratio/n}',
+            )
 
-    question = q['question']
-    context_id = q['context_id']
-    corpus = [question] + [tuple['context'] for tuple in contexts['data'][:]]
-    tokenizer = custom_tokenizer.custom_tokenizer
-    vocabulary = tokenizer(question, remove_duplicate=True)
-    vectorizer = TfidfVectorizer(
-        vocabulary=vocabulary, tokenizer=tokenizer, norm=None, binary=True
-    )
-    # print([tuple['context'] for tuple in contexts['data'][:5]])
-    x = vectorizer.fit_transform(corpus)
-    x = x.toarray()
-    # features = np.zeros((3, 2))
-    # print(features)
-
-    features = []
-    for row_idx in range(x.shape[0]):
-        features.append(utils.build_features(x[row_idx, :]))
-
-    features = np.array(features)
-    print(question)
-    print(vocabulary)
-    # print(features[:5, :])
-    indices_avg = np.argpartition(features[1:, 0], -5)[-5:]
-    indices_ratio = np.argpartition(features[1:, 1], -5)[-5:]
-    predicted_contexts_avg = [contexts['data'][idx]['context_id'] for idx in indices_avg]
-    predicted_contexts_ratio = [contexts['data'][idx]['context_id'] for idx in indices_ratio]
-    predicted_context = contexts['data'][np.argmax(features[1:, 0])]['context_id']
-    predicted_context_2 = contexts['data'][np.argmax(features[1:, 1])]['context_id']
-    # print(predicted_context)
-    # print(predicted_context_2)
-    # print(predicted_contexts_avg)
-    # print(predicted_contexts_ratio)
-    # print(context_id)
-    if context_id == predicted_context:
-        c += 1
-        pred_true = True
-    if context_id == predicted_context_2:
-        c_2 += 1
-        pred_true = True
-    if pred_true:
-        inter += 1
-    if context_id in predicted_contexts_avg:
-        print('OK')
-        c_top_avg += 1
-    else:
-        print(predicted_contexts_avg)
-        for idx in indices_avg:
-            print(x[1 + idx, :])
-        print(context_id)
-        print(x[1 + context_ids.index(context_id), :])
-    if context_id in predicted_contexts_ratio:
-        c_top_ratio += 1
-    print(f'{time.time() - start_time:.2f} sec')
-
-print(
-    f'n: {n}, ratio_avg: {c/n}, ratio_custom:{c_2/n}, ratio_inter: {inter/n}, accuracy top5 avg: {c_top_avg/n}, accuracy top5 ratio: {c_top_ratio/n}'
-)
 
 # ['test de mon algorithme', 'ceci est un test à tester', "Ma phrase de test doit être testée (et non pas tester) comme ceci, j'attends le résultat. Chanteur chanter chant ! Petit test; OK. L'apostrophe"]
--- illuin/utils/utils.py
+++ illuin/utils/utils.py
@@ -1,14 +1,23 @@
 import numpy as np
 
 
-def build_features(row):
-    """
-    Given a row of the TF-IDF matrix, return (mean, ratio_complete) of the row
-    'ratio_complete' is the ratio of non-zero terms in the row.
-    """
-    count = 0
-    for value in row:
-        if value != 0:
-            count += 1
+def build_features(tfidf_matrix):
+    """Given the (m x n) tfidf matrix, return the (m x 2) feature matrix"""
 
-    return [np.mean(row), count / row.shape[0]]
+    def _build_row_features(row):
+        """
+        Given a row of the TF-IDF matrix, return (mean, ratio_complete) of the row
+        'ratio_complete' is the ratio of non-zero terms in the row.
+        """
+        count = 0
+        for value in row:
+            if value != 0:
+                count += 1
+
+        return [np.mean(row), count / row.shape[0]]
+
+    features = []
+    for row_idx in range(tfidf_matrix.shape[0]):
+        features.append(_build_row_features(tfidf_matrix[row_idx, :]))
+
+    return np.array(features)
